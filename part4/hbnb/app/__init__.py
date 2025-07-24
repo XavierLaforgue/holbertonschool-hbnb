b43eed011a6b767ff2457a4877baf94eb7469b33
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-24 10:15:36 +0200

    app/__init__.py update
    Add CORS configuration to allow the browser to make requests to the server

--- part4/hbnb/app/__init__.py
+++ part4/hbnb/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
@@ -25,6 +26,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     # app.config['ERROR_INCLUDE_MESSAGE'] = False
+    CORS(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
